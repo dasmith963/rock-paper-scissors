bb5fb441a30539d8dd3a73f5962c9d0cb35e094a
Devon <Devon.smith017@gmail.com>
2022-11-24 00:19:52 -0600

    Create playRound function

--- js/script.js
+++ js/script.js
@@ -1,28 +1,41 @@
-function getComputerChoice(){
-  const randomNumber = Math.floor( (Math.random() * 3) + 1);
- 
-  if (randomNumber === 1){
-    return 'Rock';
-  } else if (randomNumber === 2){
-    return 'Paper';
-  } else{
-    return 'Scissors';
+let playerScore = 0
+let computerScore = 0
+
+function getComputerChoice() {
+  const randomNumber = Math.floor((Math.random() * 3) + 1);
+
+  if (randomNumber === 1) {
+    return 'rock';
+  } else if (randomNumber === 2) {
+    return 'paper';
+  } else {
+    return 'scissors';
   }
 }
 
+function playRound(playerSelection, computerSelection) {
+  playerSelection = prompt('What will you choose, Rock, Paper or Scissors?').toLowerCase();
+  computerSelection = getComputerChoice();
 
-// the user inputs their choice: rock, paper, scissors
-// check for case sensitive values 
+  if (playerSelection === '' || playerSelection === null){
+    return 'Please make a selection'
+  }else if(playerSelection === computerSelection){
+    return `It's a tie! You both picked ${playerSelection}`
+  } else if (playerSelection === 'rock' && computerSelection === 'scissors'){
+    playerScore++
+    return 'You win! Rock crushes scissors';
+  } else if (playerSelection === 'paper' && computerSelection === 'rock'){
+    playerScore++
+    return 'You win! Paper covers rock';
+  } else if (playerSelection === 'scissors' && computerSelection === 'paper'){
+    playerScore++
+    return 'You win! Scissors cuts paper';
+  } else {
+    computerScore++
+    return `You lost, ${computerSelection} beats ${playerSelection}`;
+  }
 
-// when both played an object, look to see who’s won 
-	//if Rock “crushes” scissors
- 	 //if scissors “cuts” paper 
-  	//if paper “covers” rock.
-	//if both players throw the same object, tie. 
-    // In this situation, simply play again. Continue doing this until there is a clear winner.
+}
 
-// Determine the winner
-
- // Initialize a scoreCount variable and set its value to zero
 // “best of 5” While counter is smaller than 5 increment the counter by one
 // Print the value of the counter variable

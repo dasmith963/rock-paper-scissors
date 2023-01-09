c470bd093e39ad104d312abe03cc53ce30c1e28d
Devon <Devon.smith017@gmail.com>
2023-01-08 23:38:22 -0600

    Remove update score function.

--- js/script.js
+++ js/script.js
@@ -4,20 +4,15 @@ const playerScoreBoard = document.querySelector('.player-score');
 const computerScoreBoard = document.querySelector('.computer-score');
 const roundResult = document.querySelector('.round-result');
 const finalResult = document.querySelector('.final-result');
-const newGame = document.querySelector('.new-game')
+const newGame = document.querySelector('.new-game');
+const playerCardText = document.querySelector('.player-choice-text');
+const computerCardText = document.querySelector('.computer-choice-text');
+const playerIcon = document.querySelector('.player-icon');
+const computerIcon = document.querySelector('.computer-icon');
+
 const choiceButtons = document.querySelectorAll('.player-choice');
 
 choiceButtons.forEach(button => button.addEventListener('click', playRound));
-
-newGame.addEventListener('click', () =>{
-  playerScore = 0;
-  computerScore = 0;
-  choiceButtons.forEach(button => button.disabled = false);
-  playerScoreBoard.textContent = playerScore;
-  computerScoreBoard.textContent = computerScore;
-  roundResult.textContent = 'Please Make A Selection'
-  finalResult.textContent = 'Let\'s play a game, first to score 5 points wins!'
-})
 
 function getComputerChoice() {
   const choices = ['rock', 'paper', 'scissors'];
@@ -31,28 +26,53 @@ function checkWinner(playerSelection, computerSelection) {
   } 
   else if (playerSelection === 'rock' && computerSelection === 'scissors'){
     roundResult.textContent = 'You win! Rock crushes scissors';
-    return 'player';
+    playerScoreBoard.textContent = ++playerScore;
   }
   else if (playerSelection === 'paper' && computerSelection === 'rock'){
     roundResult.textContent = 'You win! Paper covers rock';
-    return 'player';
+    playerScoreBoard.textContent = ++playerScore;
   }
    else if (playerSelection === 'scissors' && computerSelection === 'paper'){
     roundResult.textContent = 'You win! Scissors cuts paper';
-    return 'player';
+    playerScoreBoard.textContent = ++playerScore;
    }
   else {
       roundResult.textContent = `You lost, ${computerSelection} beats ${playerSelection}`;
-      return 'computer';
+      computerScoreBoard.textContent = ++computerScore;
     }
 }
 
-function updateScore(winner) {
-  if (winner === 'player') playerScoreBoard.textContent = ++playerScore;
-  if (winner === 'computer') computerScoreBoard.textContent = ++computerScore;
+function updatePlayerCard(playerSelection){
+  if (playerSelection === 'rock'){
+    playerIcon.innerHTML = '<i class="fa-regular fa-hand-back-fist fa-2xl"></i>';
+    playerCardText.textContent = 'Rock';
+  }
+  if (playerSelection === 'paper'){
+    playerIcon.innerHTML = '<i class="fa-regular fa-hand fa-2xl"></i>';
+    playerCardText.textContent = 'Paper';
+  }
+  if (playerSelection === 'scissors'){
+    playerIcon.innerHTML = '<i class="fa-regular fa-hand-scissors fa-flip-horizontal fa-2xl"></i>';
+    playerCardText.textContent = 'Scissors';
+  }
 }
 
-function declareWinner() {
+function updateComputerCard(computerSelection){
+  if (computerSelection === 'rock'){
+    computerIcon.innerHTML = '<i class="fa-regular fa-hand-back-fist fa-2xl"></i>';
+    computerCardText.textContent = 'Rock';
+  }
+  if (computerSelection === 'paper'){
+    computerIcon.innerHTML = '<i class="fa-regular fa-hand fa-2xl"></i>';
+    computerCardText.textContent = 'Paper';
+  }
+  if (computerSelection === 'scissors'){
+    computerIcon.innerHTML = '<i class="fa-regular fa-hand-scissors fa-2xl"></i>';
+    computerCardText.textContent = 'Scissors';
+  }
+}
+
+function isGameOver() {
   if (playerScore === 5) {
     finalResult.textContent = 'Congratulations!! You win!';
     choiceButtons.forEach(button => button.disabled = true);
@@ -67,8 +87,22 @@ function declareWinner() {
 function playRound(ev) {
   playerSelection = ev.target.id;
   computerSelection = getComputerChoice();
-  winner = checkWinner(playerSelection, computerSelection);
-  updateScore(winner);
-  declareWinner();
+  checkWinner(playerSelection, computerSelection);
+  updatePlayerCard(playerSelection);
+  updateComputerCard(computerSelection);
+  isGameOver();
 }
 
+newGame.addEventListener('click', () =>{
+  playerScore = 0;
+  computerScore = 0;
+  choiceButtons.forEach(button => button.disabled = false);
+  playerScoreBoard.textContent = playerScore;
+  computerScoreBoard.textContent = computerScore;
+  playerCardText.textContent = '';
+  computerCardText.textContent = '';
+  playerIcon.innerHTML = '<i class="fa-solid fa-question fa-bounce fa-2xl"></i>';
+  computerIcon.innerHTML = '<i class="fa-solid fa-question fa-bounce fa-2xl"></i>';
+  roundResult.textContent = 'Please Make A Selection';
+  finalResult.textContent = 'Let\'s play a game, first to score 5 points wins!';
+})

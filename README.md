a70684ed40d7edfab345f8330d06bfb57d37e380
Devon <Devon.smith017@gmail.com>
2023-01-09 23:50:41 -0600

    Add function to capitalize first letter of a word.

--- README.md
+++ README.md
@@ -2,3 +2,4 @@ Rock Paper Scissors
 
 This will be my first attempt at building a functional JavaScript program. The program will play a 5 round game of rock paper scissors, that will keep score, display the results of each round, and declare a winner at the end on the game. The game is only playable from the browser console at the moment, but I plan to revisit this project later
 to add a graphical user interface with buttons and text once I learn how to do so. 
+
--- js/script.js
+++ js/script.js
@@ -3,13 +3,13 @@ let computerScore = 0;
 const playerScoreBoard = document.querySelector('.player-score');
 const computerScoreBoard = document.querySelector('.computer-score');
 const roundResult = document.querySelector('.round-result');
+const matchChoices = document.querySelector('.match-choices');
 const finalResult = document.querySelector('.final-result');
 const newGame = document.querySelector('.new-game');
 const playerCardText = document.querySelector('.player-choice-text');
 const computerCardText = document.querySelector('.computer-choice-text');
 const playerIcon = document.querySelector('.player-icon');
 const computerIcon = document.querySelector('.computer-icon');
-
 const choiceButtons = document.querySelectorAll('.player-choice');
 
 choiceButtons.forEach(button => button.addEventListener('click', playRound));
@@ -20,25 +20,34 @@ function getComputerChoice() {
   return choices[randomNumber];
 }
 
+function capitalizeFirstLetter (string) {
+  return string.toUpperCase().charAt(0) + string.substring(1).toLowerCase();
+}
+
 function checkWinner(playerSelection, computerSelection) {
   if (playerSelection === computerSelection) {
-    roundResult.textContent = `It's a tie! You both picked ${playerSelection}`;
+    roundResult.textContent = 'It\'s a tie!';
+    matchChoices.textContent = `You both picked ${playerSelection}`;
   } 
   else if (playerSelection === 'rock' && computerSelection === 'scissors'){
-    roundResult.textContent = 'You win! Rock crushes scissors';
+    roundResult.textContent = 'You win!';
+    matchChoices.textContent = 'Rock crushes scissors';
     playerScoreBoard.textContent = ++playerScore;
   }
   else if (playerSelection === 'paper' && computerSelection === 'rock'){
-    roundResult.textContent = 'You win! Paper covers rock';
+    roundResult.textContent = 'You win!';
+    matchChoices.textContent = 'Paper covers rock';
     playerScoreBoard.textContent = ++playerScore;
   }
    else if (playerSelection === 'scissors' && computerSelection === 'paper'){
-    roundResult.textContent = 'You win! Scissors cuts paper';
+    roundResult.textContent = 'You win!';
+    matchChoices.textContent = 'Scissors cuts paper';
     playerScoreBoard.textContent = ++playerScore;
    }
   else {
-      roundResult.textContent = `You lost, ${computerSelection} beats ${playerSelection}`;
-      computerScoreBoard.textContent = ++computerScore;
+    roundResult.textContent = 'You lost.';
+    matchChoices.textContent = `${capitalizeFirstLetter(computerSelection)} beats ${playerSelection}`;
+    computerScoreBoard.textContent = ++computerScore;
     }
 }
 
@@ -74,12 +83,12 @@ function updateComputerCard(computerSelection){
 
 function isGameOver() {
   if (playerScore === 5) {
-    finalResult.textContent = 'Congratulations!! You win!';
+    finalResult.textContent = 'Congratulations! You won the game!';
     choiceButtons.forEach(button => button.disabled = true);
   }
   
   if (computerScore === 5){
-    finalResult.textContent = 'You lose. Would you like a rematch?';
+    finalResult.textContent = 'You lost the game. Would you like a rematch?';
     choiceButtons.forEach(button => button.disabled = true);
   }
 }
@@ -103,6 +112,10 @@ newGame.addEventListener('click', () =>{
   computerCardText.textContent = '';
   playerIcon.innerHTML = '<i class="fa-solid fa-question fa-bounce fa-2xl"></i>';
   computerIcon.innerHTML = '<i class="fa-solid fa-question fa-bounce fa-2xl"></i>';
-  roundResult.textContent = 'Please Make A Selection';
+  roundResult.textContent = '';
+  matchChoices.textContent = '';
   finalResult.textContent = 'Let\'s play a game, first to score 5 points wins!';
 })
+
+
+
